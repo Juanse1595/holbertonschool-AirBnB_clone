71dc173f1f097d8caeaa1c9ae67e77e1dce3dcd5
Juanse1595 <jspbescos15@gmail.com>
2024-03-18 14:28:33 -0500

    UPD models/base_model.py created the class BaseModel. ADD .gitignore

--- models/base_model.py
+++ models/base_model.py
@@ -19,13 +19,37 @@ class BaseModel:
         Constructor method for BaseModel class
         """
         self.id = str(uuid4())
-        self.created_at = datetime()
-        self.updated_at = datetime()
-    
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
     def __str__(self) -> str:
-        return f"[{self.__class__.__name__}]"
+        """
+        Determines what string will be displayed when print is executed on the
+        instance
+
+        Returns: The string descripting the object
+        """
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+    def save(self) -> None:
+        """
+        Updates the object
+        """
+        self.updated_at = datetime.now()
+
+    def to_dict(self) -> dict:
+        """
+        Returns a dictionary with the object key value pairs
+        """
+        object_dict = self.__dict__
+        object_dict["__class__"] = self.__class__.__name__
+        created_at_iso = datetime.isoformat(self.created_at)
+        updated_at_iso = datetime.isoformat(self.updated_at)
+        object_dict["created_at"] = created_at_iso
+        object_dict["updated_at"] = updated_at_iso
+        return object_dict
 
 
-object1 = BaseModel()
-print(object1)
+# object1 = BaseModel()
+# print(object1.to_dict())
 # print(f"id: {object1.id}, type: {type(object1.id)}")
