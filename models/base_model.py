f9958bac5794080daf2d5bdd58e6347f8fbf6ccf
Juanse1595 <jspbescos15@gmail.com>
2024-03-19 17:23:11 -0500

    UPD base_model.py added kwargs to the init method

--- models/base_model.py
+++ models/base_model.py
@@ -14,13 +14,21 @@ class BaseModel:
     BaseModel class
     Defines all common attributes and methos for the rest of the classes
     """
-    def __init__(self) -> None:
+    def __init__(self, **kwargs) -> None:
         """
         Constructor method for BaseModel class
         """
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    if key == "created_at" or key == "updated_at":
+                        setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                    else:
+                        setattr(self, key, value)
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self) -> str:
         """
@@ -53,3 +61,21 @@ class BaseModel:
 # object1 = BaseModel()
 # print(object1.to_dict())
 # print(f"id: {object1.id}, type: {type(object1.id)}")
+my_model = BaseModel()
+my_model.name = "My_First_Model"
+my_model.my_number = 89
+print(my_model.id)
+print(my_model)
+print(type(my_model.created_at))
+print("--")
+my_model_json = my_model.to_dict()
+print(my_model_json)
+print("JSON of my_model:")
+for key in my_model_json.keys():
+    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+
+print("--")
+my_new_model = BaseModel(**my_model_json)
+print(my_new_model.id)
+print(my_new_model)
+print(type(my_new_model.created_at))
